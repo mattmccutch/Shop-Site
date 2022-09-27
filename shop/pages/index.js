774a7f1f0dcf1db8dd95b88f0f6d86c3065e2b96
Matthew McCutcheon <mccma494@student.otago.ac.nz>
2022-09-27 21:42:34 +1300

    setup app

---
+++ shop/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import head from '../components/head'
+export default function Home() {
+  return (
+    <div >
+      <head></head>
+    </div>
+  )
+}
